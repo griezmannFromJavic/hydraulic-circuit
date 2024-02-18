7cfa9a7511b75ec155e86031770a30838ac39cfe
Josip Krtolačić <josip@debian.debian.com>
2024-02-18 21:15:40 +0100

    radi lagrange

--- lagrange-polynomial.scm
+++ lagrange-polynomial.scm
@@ -3,12 +3,12 @@
 		(lambda (x) (/ (- x (car other-point)) (- (car point) (car other-point))))
 		)
 	(define (list-of-factors point) ; returns list of functions of x
-		(map (lambda (p) (basis-factor point p)) (remove (lambda (p) (equal? point p)) points)) ; TU JE BUG! map mapira jednu funkciju na cijelu listu
+		(map (lambda (p) (basis-factor point p)) (remove (lambda (p) (equal? point p)) points))
 		)
 	(define (basis-polynomial point)
 		(multiply-list-of-functions (list-of-factors point))
 		)
-	(multiply-list-of-functions (map multiply-function-with-number (map cadr points) (map basis-polynomial points)))
+	(sum-list-of-functions (map multiply-function-with-number (map cadr points) (map basis-polynomial points)))
 	)
 ;; add error message if 2 x's are the same
 
--- main.scm
+++ main.scm
@@ -5,7 +5,7 @@
 (load "write-to-csv.scm")
 
 
-(define points '((0 10) (3 9) (10 0) (4 7)))
-(define xs (linspace 1 10 101))
+(define points '((0 10) (3 9) (10 0)))
+(define xs (linspace 0 10 101))
 (define ys (map (lagrange-interpolation points) xs))
 (write-csv-file "lagrange-data.csv" (list (map exact->inexact xs) (map exact->inexact ys)))
