4741d771e704d6c5332b08c655fa005c977d1572
Josip Krtolačić <josip@debian.debian.com>
2024-02-27 23:24:14 +0100

    dfs skoro radi

--- dfs.scm
+++ dfs.scm
@@ -1,15 +1,27 @@
 (define (dfs-init graph initial-node)
 "returns list of tree links"
 (define tree '())
-(define visited '(inital-node))
+(define visited (list initial-node))
 	(define (dfs node)
+	
+	(display "node")
 	(display node)
 	(newline)
+	
 	"operation for one node"
 		(define neighbours (cadr (assoc node adjacency-list-undirected)))
 		(define unvisited-neighbours (set-difference neighbours visited))
+		
+		(display "unvisited-neighbours")
 		(display unvisited-neighbours)
+		(newline)
+		
 		(set! visited (cons node visited))
+		
+		(display "visited")
+		(display visited)
+		(newline)
+		
 		(set! tree
 			(append tree
 				(map (lambda (neighbour) (list node neighbour)) unvisited-neighbours)
