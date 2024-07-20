bbf341e359a66f41d6f51c8cf9b697a800c7ab77
josip <j.krtolacic@gmail.com>
2024-07-20 23:15:10 +0200

    matrix works

--- main.scm
+++ main.scm
@@ -5,7 +5,6 @@
 (load "lagrange-polynomial.scm")
 (load "write-to-csv.scm")
 (load "matrix-generator.scm")
-(load "dfs.scm")
 
 (define working-points (map cadr system))
 (define pressure-characteristics (map lagrange-polynomial working-points))
--- matrix-generator.scm
+++ matrix-generator.scm
@@ -1,3 +1,35 @@
+(define (dfs-tree adj-list initial-node)
+	(letrec (
+		(neighbours (lambda (node) (cadr (assoc node adj-list))))
+		(dfs (lambda (node visited tree)
+			(let ((new-visited (cons node visited)))
+				(fold-right
+					(lambda (neighbor result)
+						(let ((result-tree (car result))
+							(result-visited (cdr result)))
+							(if (member neighbor result-visited)
+								result
+								(let ((dfs-result (dfs neighbor result-visited (cons (list node neighbor) result-tree))))
+									(cons (car dfs-result) (cdr dfs-result))))))
+					(cons tree new-visited)
+					(neighbours node)))))
+		)
+		(reverse (car (dfs initial-node '() '())))
+	))
+
+(define tree (dfs-tree adjacency-list-undirected 0))
+
+(define chords (set-difference
+	(set-difference links (map reverse tree))
+	tree)
+	)
+	
+#|
+(define loop (lambda (chord tree)
+	(let (
+		(
+|#
+
 ; defining INCIDENCE MATRIX
 (define (incidence-matrix graph)
 	(lambda (node link)
@@ -16,9 +48,9 @@
 
 (define (fundamental-circuit-matrix graph tree)
   (lambda (node link)
-    (let (
-          (owner (car (list-ref system link)))
-          (neighbour (cadr (list-ref system link)))
+    (letrec (
+          ; loops
+          ; loop-currents
           )
     (cond ((member (list owner neighbour) tree) 1)
           ((member (list neighbour owner) tree) -1)
