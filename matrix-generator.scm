ac74b890ee554f89ce6e0c3176bac89cb31565d1
josip <j.krtolacic@gmail.com>
2024-07-21 21:52:25 +0200

    still working on graph theory application

--- matrix-generator.scm
+++ matrix-generator.scm
@@ -1,33 +1,77 @@
+(define (neighbours node adj-list) (cadr (assoc node adj-list)))
+
+(define (tree-neighbours node tree)
+	(cond
+		((null? tree) '())
+		((equal? (car (car tree)) node)
+			(cons (cadr (car tree)) (tree-neighbours (cdr tree) node))
+			)
+		((equal? (cadr (car tree)) node)
+			(cons (car (car tree)) (tree-neighbours (cdr tree) node))
+			)
+		(else (tree-neighbours (cdr tree) node))
+		))
+
 (define (dfs-tree adj-list initial-node)
+"Creates a spanning tree in a graph using DFS. Tree is subset of links"
 	(letrec (
-		(neighbours (lambda (node) (cadr (assoc node adj-list))))
 		(dfs (lambda (node visited tree)
-			(let ((new-visited (cons node visited)))
+			(let
+				((new-visited (cons node visited)))
 				(fold-right
 					(lambda (neighbor result)
-						(let ((result-tree (car result))
-							(result-visited (cdr result)))
+						(let (
+							(result-tree (car result))
+							(result-visited (cdr result))
+								)
 							(if (member neighbor result-visited)
 								result
-								(let ((dfs-result (dfs neighbor result-visited (cons (list node neighbor) result-tree))))
-									(cons (car dfs-result) (cdr dfs-result))))))
+								(let (
+									(dfs-result (dfs neighbor result-visited (cons (list node neighbor) result-tree)))
+									)
+									(cons (car dfs-result) (cdr dfs-result))
+									))))
 					(cons tree new-visited)
-					(neighbours node)))))
+					(neighbours node adj-list))
+					)))
 		)
-		(reverse (car (dfs initial-node '() '())))
+		;(reverse (car (dfs initial-node '() '()))) ; THIS WORKS
+		(dfs initial-node '() '()) ; ONLY FOR TESTING
 	))
 
-(define tree (dfs-tree adjacency-list-undirected 0))
+(define tree0 (dfs-tree adjacency-list-undirected 0))
+(display links) (newline) (display tree0) (newline)
 
-(define chords (set-difference
-	(set-difference links (map reverse tree))
-	tree)
-	)
-	
 #|
-(define loop (lambda (chord tree)
-	(let (
-		(
+(define (chords tree) ;global variable "links" inside function!!!
+	(set-difference
+		(set-difference links (map reverse tree))
+			tree)
+		)
+	
+(define chords0 (chords tree0))
+
+
+(define tree-loop (lambda (chord tree)
+	(letrec (
+		(start (car chord))
+		(finish (cadr chord))
+		(visited (lambda () (cons node visited)))
+		(unvisited-neighbour
+			(lambda ()
+				(car (set-difference (tree-neighbours tree node) visited))
+				))
+		(dfs
+			(lambda (node neighbour visited tree)
+				(if (equal? (cdr loop-link) finish)
+				visited
+				(dfs (neighbour (unvisited-neighbour neighbour) (cons neighbour visited) tree))
+				)))
+			)
+		(dfs start tree)
+		)))
+		
+(display (tree-loop (car chords0) tree0))
 |#
 
 ; defining INCIDENCE MATRIX
