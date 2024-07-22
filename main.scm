2f03d845801f108eccf77242ae4aeac7e49300d8
griezmannFromJavic <119756145+griezmannFromJavic@users.noreply.github.com>
2024-07-22 15:02:54 +0200

    *cleanup*

--- main.scm
+++ main.scm
@@ -3,15 +3,11 @@
 (load "solver-constants.scm")
 (load "system.scm")
 (load "lagrange-polynomial.scm")
-(load "write-to-csv.scm")
+;(load "write-to-csv.scm")
 (load "matrix-generator.scm")
 
-(define working-points (map cadr system))
+(define working-points (map cadr *system*))
 (define pressure-characteristics (map lagrange-polynomial working-points))
 
 (define flows (linspace 0 2 101))
 (define pressures (map (lambda (f) (map f flows)) pressure-characteristics))
-(write-csv-file "lagrange-data.csv"
-	(map (lambda (lst) (map exact->inexact lst))
-		(cons flows pressures)
-		))
