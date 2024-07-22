89daeebce888b868e29374e8e269c7f7978550d8
griezmannFromJavic <119756145+griezmannFromJavic@users.noreply.github.com>
2024-07-22 15:00:57 +0200

    *cleanup*

--- system.scm
+++ system.scm
@@ -1,9 +1,9 @@
 ; contains information about network: the nodes of a given link and the points of a pressure characteristics
 ; ((outlet-node inlet-node) ((x1 y1) (x2 y2) ... (xn yn))
 
-(define system '(
-	((0 4) ((0 4.1) (1 3) (2 0))) ; first links
-	((5 0) ((0 0) (1 1) (2 4))) ; second link
+(define *system* '(
+	((0 4) ((0 4.1) (1 3) (2 0))) 
+	((5 0) ((0 0) (1 1) (2 4)))
 	((0 1) ((0 0) (1 1.5) (2 4)))
 	((0 2) ((0 10) (1 0) (2 10)))
 	((1 4) ((0 10) (1 8) (2 0)))
@@ -12,17 +12,17 @@
 	((1 3) ((0 0) (1 1.7) (2 4)))
 	((2 3) ((0 0) (2 8)))
 	((3 4) ((0 0) (1 2) (3 9.2)))
-	((3 5) ((0 0) (1 1) (2 4))) ; n'th link
+;	((3 5) ((0 0) (1 1) (2 4))) 
 	))
 
-(define links (map car system))
+(define *links* (map car *system*))
 
 
-(define outlet-nodes (map car links))
-(define inlet-nodes (map cadr links))
-(define num-links (length links))
-(define all-nodes (remove-duplicates (append inlet-nodes outlet-nodes)))
-(define num-nodes (length all-nodes))
+(define *outlet-nodes* (map car *links*))
+(define *inlet-nodes* (map cadr *links*))
+(define *num-links* (length *links*))
+(define *all-nodes* (remove-duplicates (append *inlet-nodes* *outlet-nodes*)))
+(define *num-nodes* (length *all-nodes*))
 
 (define (adjacency-list graph is-directed?)
 	"Creates adjacency list from graph description.
@@ -40,8 +40,8 @@
 								))
 							graph)
 						(set! neighbours (cons (list node node-neighbour) neighbours))))
-					all-nodes)
+					*all-nodes*)
 				(reverse neighbours)))
 
-(define adjacency-list-directed (adjacency-list links #t))
-(define adjacency-list-undirected (adjacency-list links #f))
+(define *adjacency-list-directed* (adjacency-list *links* #t))
+(define *adjacency-list-undirected* (adjacency-list *links* #f))
